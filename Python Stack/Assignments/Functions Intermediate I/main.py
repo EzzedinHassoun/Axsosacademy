618eeaeede4717f8117d3c7c93f1bd6d6575c735
ezzelden <ezzedinhassoun@axsosacademygit config --global user.email ezzedin.hassoun@axsos.academy>
2025-09-23 16:24:05 +0300

    assign

---
+++ Python Stack/Assignments/Functions Intermediate I/main.py
@@ -0,0 +1,24 @@
+import random
+def randInt(min=0,max=100):
+    if min>max:
+        min,max=max,min
+    if max <0:
+        max=0
+    if min<0:
+        min=0
+    
+    num=random.random()*(max-min)+min
+    return round(num)
+
+print(randInt())
+# should print a random integer between 0 to 100
+print(randInt(max=50))
+# should print a random integer between 0 to 50
+print(randInt(min=50))
+# should print a random integer between 50 to 100
+print(randInt(min=50, max=500))
+# should print a random integer between 50 and 500
+
+# for bounus
+print(randInt(min=-5, max=50)) 
+print(randInt(max=-3))
